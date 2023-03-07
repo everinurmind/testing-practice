d9e80bcfaa889457c66309666e136ce4f77e82ed
everinurmind <nurbol.sultanov@gmail.com>
2023-03-07 16:08:17 -0600

    add task 1

---
+++ tasks/task1.js
@@ -0,0 +1,9 @@
+function stringLength(string) {
+    if (string.length < 1 || string.length > 10) {
+      throw new Error('String length should be between 1 and 10')
+    } else {
+      return string.length;
+    }
+  }
+  
+  module.exports = stringLength;
---
+++ tasks/task1.test.js
@@ -0,0 +1,12 @@
+test('length of string "hello" should be between 1 and 10', () => {
+    // Arrange
+    const stringLength = require('./task1.js');
+  
+    // Act
+    let min = 1;
+    let max = 10;
+  
+    // Assert
+    expect(stringLength('hello')).toBeGreaterThanOrEqual(min);
+    expect(stringLength('hello')).toBeLessThanOrEqual(max);
+  });
